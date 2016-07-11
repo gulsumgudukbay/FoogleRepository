02d52de28b1b93602ddde48b37f327490dac4c75
Muhammed Cavusoglu <mcavus495@gmail.com>
2016-07-11 18:15:37 +0300

    Add methods and properties to Food.java
    Missing ones are stated as "TODO"

--- Foogle/src/restaurantAndFoodManagement/Food.java
+++ Foogle/src/restaurantAndFoodManagement/Food.java
@@ -1,5 +1,55 @@
 package restaurantAndFoodManagement;
 
 public class Food {
-
+	// MARK: Properties
+	private String name;
+	private ArrayList<Ingredient> ingredients;
+	private Double price
+	private OtheringProcessor oip;
+	private String otheringBuff; 
+	// TODO: enum type & cuisine
+	
+	// MARK: Constructors
+	public Food(String name, Double price) {
+		this.name = name;
+		this.price = price;
+	}
+	public Food() {
+		this.name = "";
+		this.price = 0.0;
+	}
+	
+	// MARK: Methods
+	// Accessors and Mutators
+	public String getName() {
+		return this.name
+	}
+	public Double getPrice() {
+		return this.price
+	}
+	// TODO: get method for ingredients
+	public void setName(String name) {
+		this.name = name
+	}
+	public void setPrice(Double price) {
+		this.price = price
+	}
+	// TODO: set method for ingredients
+	
+	// Utility methods
+	// Finds the ingredient with given name, in ingredients array list and returns it
+	// TODO: test this method after creating Ingredient
+	public Ingredient getIngredient(String name) {
+		boolean found = false;
+		for(Ingredient temp: ingredients) {
+			if(temp.name == name) {
+				found = true;
+				return temp;
+			}
+		}
+		if(!found) {
+			return null;
+		}
+	}
+	// TODO: insertToPending(otheringBuf)
 }
