8ad72487e0afc5fd02ca484b7157d6509665738f
Muhammed Cavusoglu <mcavus495@gmail.com>
2016-07-11 18:57:57 +0300

    Finish Ingredient.java and make minor changes in Food.java
    Test for Food.java is pending

--- Foogle/src/restaurantAndFoodManagement/Food.java
+++ Foogle/src/restaurantAndFoodManagement/Food.java
@@ -1,22 +1,27 @@
 package restaurantAndFoodManagement;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Food {
 	// MARK: Properties
 	private String name;
+	private Double price;
 	private ArrayList<Ingredient> ingredients;
-	private Double price
-	private OtheringProcessor oip;
-	private String otheringBuff; 
+	// TODO: private OtheringProcessor oip;
+	// TODO: private String otheringBuff; 
 	// TODO: enum type & cuisine
 	
 	// MARK: Constructors
-	public Food(String name, Double price) {
-		this.name = name;
-		this.price = price;
+	public Food(String name, Double price, ArrayList<Ingredient> ingredients) {
+		this.setName(name);
+		this.setPrice(price);
+		this.ingredients = ingredients;
 	}
 	public Food() {
 		this.name = "";
 		this.price = 0.0;
+		this.ingredients = new ArrayList<Ingredient>();
 	}
 	
 	// MARK: Methods
@@ -27,14 +32,18 @@ public class Food {
 	public Double getPrice() {
 		return this.price
 	}
-	// TODO: get method for ingredients
+	public ArrayList<Ingredient> getIngredients() {
+		return this.ingredients
+	}
 	public void setName(String name) {
 		this.name = name
 	}
 	public void setPrice(Double price) {
 		this.price = price
 	}
-	// TODO: set method for ingredients
+	public void setIngredients(ArrayList<Ingredient> ingredients) {
+		this.ingredients = ingredients
+	}
 	
 	// Utility methods
 	// Finds the ingredient with given name, in ingredients array list and returns it
@@ -44,12 +53,35 @@ public class Food {
 		for(Ingredient temp: ingredients) {
 			if(temp.name == name) {
 				found = true;
+				// TEST
+				System.out.println("Found: " + name);
 				return temp;
 			}
 		}
 		if(!found) {
+			System.out.println("Ingredient " + name + " not found.")
 			return null;
 		}
 	}
 	// TODO: insertToPending(otheringBuf)
+	
+	// TEST
+	public static void main(String[] args) {
+		Ingredient myTomato = new Ingredient("tomato");
+		Ingredient myChicken = new Ingredient("chicken");
+		Ingredient myFries = new Ingredient("fries");
+		
+		ArrayList<Ingredient> myIngredients = new ArrayList<Ingredient>();
+		myIngredients.add(myTomato);
+		myIngredients.add(myChicken);
+		myIngredients.add(myFries);
+		
+		Food myFood = new Food("donner", 10.0, myIngredients);
+		
+		System.out.println(myFood.getName());
+		System.out.println(myFood.getPrice());
+		System.out.println(myFood.getIngredients());
+		
+		myFood.getIngredient("tomato");
+	}
 }
