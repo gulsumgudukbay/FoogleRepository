0f704ed14f3ee7b5c140f9cce8351e97cdd5a476
gulsumgudukbay <gulsum.gudukbay@ug.bilkent.edu.tr>
2016-07-15 02:27:12 +0300

    syntax in pendingdb

--- Foogle/src/dataManagement/PendingDB.java
+++ Foogle/src/dataManagement/PendingDB.java
@@ -5,7 +5,7 @@ import java.sql.Statement;
 public class PendingDB {
 	
 	private DatabaseManager dbm = DatabaseManager.getSoleInstance();
-	private Statement stmt = DatabaseManager.getStmt();
+	private Statement stmt = DatabaseManager.createStmt();
 	private static PendingDB pdb = new PendingDB();
 	
 	public static PendingDB getSoleInstance(){
