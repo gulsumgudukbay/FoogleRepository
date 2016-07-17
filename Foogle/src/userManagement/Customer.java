ebd147c573f7e9dab10408dcbee48e9bc429bc5f
Muhammed Cavusoglu <mcavus495@gmail.com>
2016-07-17 14:30:32 +0300

    Additions to Customer.java
    Unclear methods

--- Foogle/src/userManagement/Customer.java
+++ Foogle/src/userManagement/Customer.java
@@ -11,11 +11,18 @@ public class Customer extends User {
 	RestDB rdb = RestDB.getSoleInstance();
 	SearchController sc = new SearchController();
 	
+	// TODO: Uncomment the property below
+	// private Event input;
+	
 	public ArrayList<Ingredient> getAllIngredients(){
 		return rdb.getAllIngredients();
 	}
 	
 	public ArrayList<Food> search(ArrayList<String> wanted, ArrayList<String> unwanted, String types){
 		return sc.showResults(wanted, unwanted, types);
-	}	
+	}
+	
+	// FIXME: Login and Sign Up
+	// How do we login and sign up user? Customer doesn't have username or anything. 
+	// Where should we write these methods? Where should we call them?
 }		
