74dbfaab7b3ece360167e893e687ee5a436169ec
gulsumgudukbay <gulsum.gudukbay@ug.bilkent.edu.tr>
2016-07-18 19:45:40 +0300

    mysql connector path is updated

--- Foogle/src/dataManagement/RestDB.java
+++ Foogle/src/dataManagement/RestDB.java
@@ -518,7 +518,7 @@ public class RestDB {
 		ings.add(ing1);
 		ings.add(ing2);
 
-		rdb.createFoodToExistingRestaurant("testRestaurant", "test2", "CREATEDFOOD", "meal", "Chinese", 19.99, ings);
+		System.out.println(rdb.createFoodToExistingRestaurant("testRestaurant", "test2", "CREATEDFOOD", "meal", "Chinese", 19.99, ings));
 
 		rdb.getFoodList(wanted, unwanted, "meal");
 
