c6b8cf004140d20f35398cdeee26dcbd6a54275d
Muhammed Cavusoglu <mcavus495@gmail.com>
2016-07-20 20:56:13 +0300

    Additions to AdminScreen.java

--- Foogle/src/uiManagement/AdminScreen.java
+++ Foogle/src/uiManagement/AdminScreen.java
@@ -1,5 +1,58 @@
 package uiManagement;
 
-public class AdminScreen {
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 
+import javax.swing.AbstractListModel;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.SpinnerNumberModel;
+
+import restaurantAndFoodManagement.Food;
+import restaurantAndFoodManagement.Ingredient;
+import restaurantAndFoodManagement.Restaurant;
+import userManagement.RestaurantOwner;
+import userManagement.UserResource;
+
+public class AdminScreen extends JFrame {
+	
+	private JPanel contentPane;
+	// TODO: Properties
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					// TODO: Fix the constructor
+					AdminScreen frame = new AdminScreen();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	// TODO: Create constructor
+	
+	// TODO: Initialize components method
 }
