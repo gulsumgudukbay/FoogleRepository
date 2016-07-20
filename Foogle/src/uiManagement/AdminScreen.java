b6de01a09c79b35300f91c3ea14b83afd266465d
Chosenwizard <mustafaduymush@gmail.com>
2016-07-20 23:02:16 +0300

    Admin Screen düzenleme

--- Foogle/src/uiManagement/AdminScreen.java
+++ Foogle/src/uiManagement/AdminScreen.java
@@ -2,38 +2,33 @@ package uiManagement;
 
 import java.awt.Color;
 import java.awt.EventQueue;
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
-import javax.swing.AbstractListModel;
-import javax.swing.DefaultComboBoxModel;
+import javax.swing.DefaultListModel;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
-import javax.swing.JSpinner;
-import javax.swing.JTextField;
 import javax.swing.LayoutStyle.ComponentPlacement;
-import javax.swing.SpinnerNumberModel;
+import javax.swing.border.EmptyBorder;
 
-import restaurantAndFoodManagement.Food;
 import restaurantAndFoodManagement.Ingredient;
 import restaurantAndFoodManagement.Restaurant;
-import userManagement.RestaurantOwner;
-import userManagement.UserResource;
+import userManagement.Admin;
 
 public class AdminScreen extends JFrame {
-	
+
+	Admin a = Admin.getSoleInstance();
 	private JPanel contentPane;
-	// TODO: Properties
+	private ArrayList<Restaurant> pendingResList = new ArrayList<Restaurant>();
+	private ArrayList<Ingredient> pendingIngList = new ArrayList<Ingredient>();
+	private JList ingList;
+	private JList resList;
 	
 	/**
 	 * Launch the application.
@@ -42,7 +37,6 @@ public class AdminScreen extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					// TODO: Fix the constructor
 					AdminScreen frame = new AdminScreen();
 					frame.setVisible(true);
 				} catch (Exception e) {
@@ -51,8 +45,136 @@ public class AdminScreen extends JFrame {
 			}
 		});
 	}
+
+	/**
+	 * Create the frame.
+	 */
+	public AdminScreen() {
+		initComponents();
+		pendingIngList = a.getPendingIngredients();
+		pendingResList = a.getPendingRestaurants();
+	}
 	
-	// TODO: Create constructor
-	
-	// TODO: Initialize components method
+	public void initComponents() {
+		
+		final DefaultListModel ingModel = new DefaultListModel();
+		for (int i = 0; i < pendingIngList.size(); i++) {
+		  ingModel.addElement(pendingIngList.get(i));
+		}
+		ingList = new JList(ingModel);
+		
+		final DefaultListModel resModel = new DefaultListModel();
+		for (int i = 0; i < pendingResList.size(); i++) {
+		  ingModel.addElement(pendingResList.get(i));
+		}
+		ingList = new JList(ingModel);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("My Account");
+		setBounds(200, 200, 1440, 800);
+		
+		setResizable(false);
+		getContentPane().setLayout(null);
+		getContentPane().setBackground(new Color(250, 240, 230));
+		
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+				
+		JButton btnRejectIng = new JButton("Reject");
+		btnRejectIng.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Ingredient currentIng = (Ingredient) ingList.getSelectedValue();
+				int i = ingList.getSelectedIndex();
+				currentIng.setConfirmed(false);
+				ingModel.remove(i);
+			}
+		});
+		
+		JButton btnConfirmIng = new JButton("Confirm");
+		btnConfirmIng.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				Ingredient currentIng = (Ingredient) ingList.getSelectedValue();
+				int i = ingList.getSelectedIndex();
+				currentIng.setConfirmed(true);
+				ingModel.remove(i);
+			}
+		});
+		
+		JButton btnRejectRes = new JButton("Reject");
+		btnRejectRes.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Restaurant currentRes = (Restaurant) resList.getSelectedValue();
+				int i = resList.getSelectedIndex();
+				currentRes.setConfirmed(false);
+				resModel.remove(i);
+			}
+		});
+		
+		JButton btnConfirmRes = new JButton("Confirm");
+		btnConfirmRes.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Restaurant currentRes = (Restaurant) resList.getSelectedValue();
+				int i = resList.getSelectedIndex();
+				currentRes.setConfirmed(true);
+				resModel.remove(i);
+			}
+		});
+		
+		JLabel lblIngredient = new JLabel("Ingredients:");
+		
+		JLabel lblRestaurants = new JLabel("Restaurants:");
+		
+		
+		
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(282)
+							.addComponent(btnConfirmIng)
+							.addGap(18)
+							.addComponent(btnRejectIng)
+							.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+							.addComponent(btnConfirmRes)
+							.addGap(18)
+							.addComponent(btnRejectRes))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addContainerGap()
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(ingList, GroupLayout.PREFERRED_SIZE, 471, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblIngredient, GroupLayout.PREFERRED_SIZE, 78, GroupLayout.PREFERRED_SIZE))
+							.addGap(181)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblRestaurants, GroupLayout.PREFERRED_SIZE, 94, GroupLayout.PREFERRED_SIZE)
+								.addComponent(resList, GroupLayout.PREFERRED_SIZE, 471, GroupLayout.PREFERRED_SIZE))))
+					.addContainerGap(301, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(4)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblIngredient)
+						.addComponent(lblRestaurants))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+						.addComponent(resList, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(ingList, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 641, Short.MAX_VALUE))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnConfirmIng)
+						.addComponent(btnRejectIng)
+						.addComponent(btnRejectRes)
+						.addComponent(btnConfirmRes))
+					.addGap(60))
+		);
+		contentPane.setLayout(gl_contentPane);
+	}
 }
